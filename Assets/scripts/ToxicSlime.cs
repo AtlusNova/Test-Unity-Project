2498625a10df372051e18cf0d5bc93ae023906ac
AtlusNova <atlusnova@gmail.com>
2021-10-14 10:34:06 -0400

    enemy added

---
+++ Assets/scripts/ToxicSlime.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToxicSlime : MonoBehaviour
+{
+    
+    public GameObject explosionEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 particleSpawnPoint = other.transform.position;
+        Instantiate(explosionEffect, particleSpawnPoint, Quaternion.identity);
+
+        print("touch");
+    }
+
+}
